edd5cc9e27b0cfffdb5a0cf8bae39ee8d31994e4
FH <FH@192.168.0.196>
2011-05-04 17:29:16 +0200

    Textausgabe in Konsole hinzugefügt

---
+++ src/start.java
@@ -0,0 +1,12 @@
+
+public class start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Git in Eclipse");
+	}
+
+}
