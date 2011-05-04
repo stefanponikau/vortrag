10a0e00285ef0f47274ba8ef617af65f85d0357b
FH <FH@192.168.0.196>
2011-05-04 23:13:59 +0200

    Neue Konsolenausgabe hinzugefügt

--- src/start.java
+++ src/start.java
@@ -6,6 +6,7 @@ public class start {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Beispiel1");
 System.out.println("Git in Eclipse");
 	}
 
