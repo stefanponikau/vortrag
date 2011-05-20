e92aa1b6f666a07aed59086e78bd377b39d5e03e
FH <FH@192.168.0.199>
2011-05-20 21:59:50 +0200

    Branche Beispel_git Beispiel3

--- src/start.java
+++ src/start.java
@@ -6,8 +6,9 @@ public class start {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Beispiel1");
-System.out.println("Git in Eclipse");
+		System.out.println("Vortrag vom 23.05.2011 ");
+		System.out.println("Beispiel2");
+		System.out.println("Git in Eclipse");
 	}
 
 }
