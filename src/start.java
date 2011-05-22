25117d7f1f4b35a6d854c921d98dc28a560c26a6
FH <FH@192.168.0.199>
2011-05-22 17:39:51 +0200

    Revert "Neue Konsolenausgabe hinzugefügt"
    This reverts commit 0434841953c8c7e05340f397fffdac39f27c91a5

--- src/start.java
+++ src/start.java
@@ -6,7 +6,6 @@ public class start {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Beispiel1");
 System.out.println("Git in Eclipse");
 	}
 
